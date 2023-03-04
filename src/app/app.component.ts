e3a94cb783d35315edbe2cf97b9b557ef28b2801
DELL <DELL@SEMIH>
2023-03-04 14:42:20 +0300

    ToastrService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ declare var $: any
 })
 export class AppComponent {
   title = 'ETicaretClient';
+  constructor(private toastrService: CustomToastrService) {
+    toastrService.message("Mesajimiz", "Title", {
+      messageType: ToastrMessageType.Error,
+      position: ToastrPosition.BottomCenter
+    });
+    toastrService.message("Mesajimiz", "Title", {
+      messageType: ToastrMessageType.Info,
+      position: ToastrPosition.BottomFullWidth
+    });
+    toastrService.message("Mesajimiz", "Title", {
+      messageType: ToastrMessageType.Success,
+      position: ToastrPosition.TopRight
+    });
+    toastrService.message("Mesajimiz", "Title", {
+      messageType: ToastrMessageType.Warning,
+      position: ToastrPosition.TopFullWidth
+    });
+  }
 }
